a3b73a112a5973470d5f1fc301b5aae1c1dad170
Illia Sakharau <sakharovia0204@gmail.com>
2024-01-27 22:42:40 +0400

    feat: implement streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from 'fs';
+import path, {dirname} from 'path';
+import { fileURLToPath } from "url";
+import { stdin } from "process";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files/fileToWrite.txt')
+
+    const writeStreamToFile = fs.createWriteStream(filePath);
+    stdin.pipe(writeStreamToFile)
 };
 
 await write();
