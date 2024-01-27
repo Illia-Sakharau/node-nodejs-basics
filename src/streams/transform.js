32215009587e24bf160a64f227483dc9414ed6fe
Illia Sakharau <sakharovia0204@gmail.com>
2024-01-27 23:15:23 +0400

    feat: implement streams:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { stdin, stdout } from "process";
+import { Transform, pipeline } from "stream";
+
+const reverseText = (text) => text.split('').reverse().join('')
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversedText = reverseText(chunk.toString()).slice(1) + '\n'
+            callback(null, reversedText);
+        },
+      })
+
+    stdin.pipe(reverseTransform).pipe(stdout)
 };
 
 await transform();
