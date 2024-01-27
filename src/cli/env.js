056958ce947399b6ce005cd66dadd9806c7cf29c
Illia Sakharau <sakharovia0204@gmail.com>
2024-01-27 18:56:40 +0400

    feat: implement cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+import process from 'process';
+
 const parseEnv = () => {
-    // Write your code here 
+    let res = '';
+    for (let key in process.env) {
+        if (key.startsWith('RSS_')) {
+            res = res + `${key}=${process.env[key]}; `
+        }
+    }
+    console.log(res.slice(0, -2));
 };
 
 parseEnv();
